fa0e6124d2a0fd5a29365e67196b12789c42d4fb
ax6030 <138182326+ax6030@users.noreply.github.com>
2023-06-30 11:06:57 +0800

    加入專案檔案。

---
+++ Faucet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaucetAndTableAndHinge
+{
+    internal class Faucet
+    {
+        public Faucet()
+        {
+            Table wine = new Table();
+            Hinge book = new Hinge();
+            wine.Set(book);
+            book.Set(wine);
+            book.garden.Lamp("back in");
+            book.bulb *= 2;
+            book.Lamp("minutes");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace FaucetAndTableAndHinge
+{
+    internal class Program
+    {
+        static void Main(string[] args) => new Faucet();
+    }
+}
